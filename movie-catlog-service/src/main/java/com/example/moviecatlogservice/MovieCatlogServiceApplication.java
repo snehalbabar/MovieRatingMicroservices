fa84836be8976ba51428f082b667ac1095648b1a
snehalbabar <h728e344@wichita.edu>
2023-11-14 16:50:18 -0500

    created three services for project

---
+++ movie-catlog-service/src/main/java/com/example/moviecatlogservice/MovieCatlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviecatlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatlogServiceApplication.class, args);
+	}
+
+}
